8eceeb99a463e85ff6a1c0316213f9d84249eedd
Vishwa Udayanga <udhayangavishwa@gmail.com>
2024-05-11 13:26:43 +0530

    shehan-ui

---
+++ app/src/main/java/com/example/magentastreaming/Login.java
@@ -0,0 +1,38 @@
+package com.example.magentastreaming;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    Button btnStartAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnStartAccount = findViewById(R.id.btnStartAccount);
+
+        btnStartAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnStartAccountTapped();
+            }
+        });
+
+    }
+
+    private void btnStartAccountTapped() {
+        Intent intent = new Intent(getApplicationContext(), Register.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/magentastreaming/Register.java
+++ app/src/main/java/com/example/magentastreaming/Register.java
@@ -8,11 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
