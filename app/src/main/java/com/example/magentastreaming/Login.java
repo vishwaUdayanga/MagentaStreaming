b1c3638b6dd2e41d5e8dee226e14d6fea46ac93e
Vishwa Udayanga <udhayangavishwa@gmail.com>
2024-05-12 16:09:56 +0530

    shehan-backend

--- app/src/main/java/com/example/magentastreaming/Login.java
+++ app/src/main/java/com/example/magentastreaming/Login.java
@@ -3,18 +3,48 @@ package com.example.magentastreaming;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
 
+<<<<<<< Updated upstream
     Button btnStartMagenta;
+=======
+//    Button btnStartAccount;
+    TextInputEditText editTextEmail,editTextPassword;
+    Button buttonLogin;
+    FirebaseAuth mAuth;
+    Button textView;
+>>>>>>> Stashed changes
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -22,19 +52,86 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+<<<<<<< Updated upstream
         btnStartMagenta = findViewById(R.id.btnStartMagenta);
 
         btnStartMagenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btnStartMagentaTapped();
+=======
+//        btnStartAccount = findViewById(R.id.btnStartAccount);
+
+//        btnStartAccount.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btnStartAccountTapped();
+//            }
+//        });
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.txtEmailEdit);
+        editTextPassword = findViewById(R.id.txtCreatePasswordEdit);
+        buttonLogin = findViewById(R.id.btnStartMagenta);
+        textView = findViewById(R.id.btnRegisterNow);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Login.this,"Enter Email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Login.this,"Enter password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Login.this, "Login Successful",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+
+                                    Toast.makeText(Login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+>>>>>>> Stashed changes
             }
         });
 
     }
 
+<<<<<<< Updated upstream
     private void btnStartMagentaTapped() {
         Intent intent = new Intent(getApplicationContext(), Register.class);
         startActivity(intent);
     }
+=======
+//    private void btnStartAccountTapped() {
+//        Intent intent = new Intent(getApplicationContext(), Register.class);
+//        startActivity(intent);
+//    }
+>>>>>>> Stashed changes
 }
--- app/src/main/java/com/example/magentastreaming/Register.java
+++ app/src/main/java/com/example/magentastreaming/Register.java
@@ -3,24 +3,61 @@ package com.example.magentastreaming;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+<<<<<<< Updated upstream
 import android.view.View;
 import android.widget.Button;
+=======
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+>>>>>>> Stashed changes
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Register extends AppCompatActivity {
 
+<<<<<<< Updated upstream
     Button btnStartAccount;
 
+=======
+    TextInputEditText editTextEmail,editTextPassword;
+    Button buttonReg;
+    FirebaseAuth mAuth;
+    Button textView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+>>>>>>> Stashed changes
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+<<<<<<< Updated upstream
 
         btnStartAccount = findViewById(R.id.btnStartAccount);
 
@@ -35,5 +72,57 @@ public class Register extends AppCompatActivity {
     private void btnStartAccountTapped() {
         Intent intent = new Intent(getApplicationContext(), AppHolder.class);
         startActivity(intent);
+=======
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.txtEmailEditRej);
+        editTextPassword = findViewById(R.id.txtCreatePasswordEditRej);
+        buttonReg = findViewById(R.id.btnStartAccount);
+        textView = findViewById(R.id.loginNow);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Register.this,"Enter Email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Register.this,"Enter password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), AppHolder.class);
+                                    startActivity(intent);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+>>>>>>> Stashed changes
     }
 }
